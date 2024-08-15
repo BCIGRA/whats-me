21b3d33bbe3b3780525ee6acbc6a053c9ec44963
BCIGRA <148233217+BCIGRA@users.noreply.github.com>
2024-08-15 18:25:39 +0700

    sessionRoutes.ts

--- src/routes/sessionRoutes.ts
+++ src/routes/sessionRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { initializeClient, getQrCode } from '../controllers/sessionController';
+import { initializeClient, getQrCode, statusClient } from '../controllers/sessionController';
 
 const router = Router();
 
 router.post('/initialize/:number', initializeClient);
-router.get('/qrcode/:number', getQrCode);
+router.post('/qrcode/:number', getQrCode);
+router.post('/status/:number', statusClient);
 
 export default router;
