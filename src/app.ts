30c2c90df26889baabf5fdc18efed61f6c2962ca
BCIGRA <148233217+BCIGRA@users.noreply.github.com>
2024-08-15 18:23:44 +0700

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';  // Import cors
+import path from 'path';
 import routerSession from './routes/sessionRoutes';
 import messageRoutes from './routes/messageRoutes';
 import groupRoutes from './routes/groupRoutes';
@@ -15,8 +16,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); // Gunakan cors dengan konfigurasi
 app.use(express.json());
+
+// Middleware untuk file statis
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// Rute API
 app.use('/api/v1.0', routerSession);
 app.use('/api/v1.0', messageRoutes);
 app.use('/api/v1.0', groupRoutes);
+
+// Middleware untuk menangani permintaan yang tidak cocok dengan rute API
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 export default app;
